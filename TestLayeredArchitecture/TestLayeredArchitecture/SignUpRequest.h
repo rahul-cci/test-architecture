294cc19cdbf8ba446ce3e2130f6c82d1522d0a26
Prachi Ghatwal <prachi.ghatwal@creativecapsule.com>
2014-11-07 16:33:52 +0530

    seperating request, response object

---
+++ TestLayeredArchitecture/TestLayeredArchitecture/SignUpRequest.h
@@ -0,0 +1,13 @@
+//
+//  SignUpRequest.h
+//  TestLayeredArchitecture
+//
+//  Created by Prachi Ghatwal on 07/11/14.
+//  Copyright (c) 2014 CCI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SignUpRequest : NSObject
+
+@end
---
+++ TestLayeredArchitecture/TestLayeredArchitecture/SignUpRequest.m
@@ -0,0 +1,13 @@
+//
+//  SignUpRequest.m
+//  TestLayeredArchitecture
+//
+//  Created by Prachi Ghatwal on 07/11/14.
+//  Copyright (c) 2014 CCI. All rights reserved.
+//
+
+#import "SignUpRequest.h"
+
+@implementation SignUpRequest
+
+@end
